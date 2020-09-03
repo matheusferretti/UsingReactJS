c6f8239074969758f6213520bfb2eed22f1ddba6
Matheus Monteiro <matheusferretti96@gmail.com>
2020-09-03 01:10:23 +0000

    updated2.1

--- exercises/01.3-rendering-from-objects/index.js
+++ exercises/01.3-rendering-from-objects/index.js
@@ -9,8 +9,8 @@ const customer = {
 //              your code inside these <div> tags
 const output = 
     <div>
-        <h1>My name is {customer.first_name}</h1>
-        <h2>My last name is {customer.last_name}</h2>
+        <h1>My name is{customer.first_name}</h1>
+        <h2>My last name is{customer.last_name}</h2>
     </div>;
 
 
--- exercises/01.5-building-html-from-arrays/index.js
+++ exercises/01.5-building-html-from-arrays/index.js
@@ -5,14 +5,14 @@ import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 const navlinkItems = [
     <li className="nav-item">
         <a className="nav-link" href="#">Link to google.com</a>
-    </li>
+    </li>,
     <li className="nav-item">
         <a className="nav-link" href="#">Link to facebook.com</a>
-    </li>
+    </li>,
     <li className="nav-item">
         <a className="nav-link" href="#">Link to amazon.com</a>
     </li>
-    ];
+];
 
 
 const content = <ul className="nav">{navlinkItems}</ul>;
